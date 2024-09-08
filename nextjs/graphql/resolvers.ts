15a0e080e0fdf118726daed7c4524996d5127be5
kyleuniversities <kyleuniversities@gmail.com>
2024-09-07 22:59:36 -0700

    deleted nextjs

--- nextjs/graphql/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { users } from "../data/users";
-import prisma from "../lib/prisma";
-
-export const resolvers = {
-  Query: {
-    users: async (_parent, args, context) =>
-      await context.prisma.user.findMany(),
-  },
-};
