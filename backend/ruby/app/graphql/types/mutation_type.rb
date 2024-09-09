c941deee9ab7d30c2a36032b47eeabb60b597745
kyleuniversities <kyleuniversities@gmail.com>
2024-09-09 07:13:15 -0700

    added creation graph queries

--- backend/ruby/app/graphql/types/mutation_type.rb
+++ backend/ruby/app/graphql/types/mutation_type.rb
@@ -2,11 +2,44 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+      field :createUser, UserType, null: false do
+        argument :name, String, required: true
+        argument :username, String, required: true
+        argument :bio, String, required: true
+      end
+      def createUser(name:,username:,bio:)
+        User.create name: name, username: username, bio: bio
+      end
+
+      field :createPost, PostType, null: false do
+        argument :userId, String, required: true
+        argument :body, String, required: true
+      end
+      def createPost(userId:,body:)
+        post = Post.create body: body
+        user = User.where(id: userId).first
+        userPosts = user.posts
+        userPosts.append(post)
+        user.update(posts: userPosts)
+        post
+      end
+
+      field :createComment, CommentType, null: false do
+        argument :userId, String, required: true
+        argument :postId, String, required: true
+        argument :body, String, required: true
+      end
+      def createComment(userId:,postId:,body:)
+        comment = Comment.create body: body
+        post = Post.where(id: postId).first
+        user = User.where(id: userId).first
+        postComments = post.comments
+        userComments = user.comments
+        postComments.append(comment)
+        userComments.append(comment)
+        post.update(comments: postComments)
+        user.update(comments: userComments)
+        comment
+      end
   end
 end
